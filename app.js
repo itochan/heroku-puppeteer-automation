250dd27be63bd3288e3a7ab2c819800fb5f3c858
Kazunori Jo <519604+itochan@users.noreply.github.com>
2020-07-19 18:04:23 +0900

    Cause lint error

--- app.js
+++ app.js
@@ -14,3 +14,5 @@ if (user && password) {
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+const unusedConst = "heeeey"
