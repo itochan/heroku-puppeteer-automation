fab42dadba157faee7915c5b1f2abc34fe092102
Kazunori Jo <519604+itochan@users.noreply.github.com>
2020-07-19 18:06:15 +0900

    Revert "Cause lint error"
    This reverts commit 250dd27be63bd3288e3a7ab2c819800fb5f3c858.

--- app.js
+++ app.js
@@ -14,5 +14,3 @@ if (user && password) {
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
-const unusedConst = "heeeey"
